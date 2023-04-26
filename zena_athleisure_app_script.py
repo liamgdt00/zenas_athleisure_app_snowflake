50284f0942a912b7b5cf65d8e655bf3f42541231
liamgdt00 <122754086+liamgdt00@users.noreply.github.com>
2023-04-26 12:12:37 +0100

    testing things

--- zena_athleisure_app_script.py
+++ zena_athleisure_app_script.py
@@ -27,18 +27,22 @@ print(color_list)
 
 # Let's put a pick list here so they can pick the color
 option = streamlit.selectbox('Pick a sweatsuit color or style:', color_list)
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+option1 = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
 
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+product_caption1 = 'Our warm, comfortable, ' + option1 + ' sweatsuit!'
 
 # use the option selected to go back and get all the info from the database
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where\
 color_or_style = '" + option + "';")
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where\
+color_or_style = '" + option1 + "';")
                
 df2 = my_cur.fetchone()
                
 streamlit.image(df2[0],width=400,caption= product_caption)
+streamlit.image(df2[0],width=400,caption= product_caption1)
 streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
